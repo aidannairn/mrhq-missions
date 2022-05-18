a61955a7d65b2d381ab9ab4dac2e1e9e6b42bfbb
aidannairn <aidan.nairn95@gmail.com>
2022-05-19 11:40:54 +1200

    Refactor code. Use JS to set dynamically set themes using inline styling. Add comments to explain functions.

--- mission-2/app.js
+++ mission-2/app.js
@@ -1,81 +1,68 @@
-let themeWrapper = ''
-let bodyHTML = ''
-// let theme = ''
+// Function is given the theme name as a parameter. If the theme name exists in an object in the themes.js file it will set the theme colors accordingly. It will also save the theme name to local storage so that the theme persists after page refresh.
 
-const changeTheme = (theme) => {
-  themeWrapper = document.getElementById('theme')
-  themeWrapper.className = theme
-  localStorage.setItem('userTheme', theme)
+const applyTheme = (themeName) => {
+  const themeWrapper = document.getElementById('theme-wrapper')
+
+  for (let i = 0; i < themes.length; i++) {
+    const theme = themes[i]
+
+    if (theme.name === themeName) {
+      themeWrapper.style.backgroundColor = theme.bgPrimary
+      themeWrapper.style.borderColor = theme.bgSecondary
+      themeWrapper.style.color = theme.colorPrimary
+
+      localStorage.setItem('userTheme', themeName)
+    }
+  }
+  return themeWrapper
 }
+
+// When the body element loads/reloads this function will check to see whether a theme has been saved to localStorage. If it has - apply that theme by retrieving the theme name and using it as a parameter for the applyTheme function. If not - set the theme name to 'default' and call the applyTheme function.
 
 const checkTheme = () => {
+  let themeName
+
   if (localStorage.userTheme != null) {
-  themeWrapper = document.getElementById('theme')
-  let theme = localStorage.getItem('userTheme')
-  themeWrapper.className = theme
-  } 
+    themeName = localStorage.getItem('userTheme')
+  } else {
+    themeName = 'default'
+  }
+
+  applyTheme(themeName)
 }
 
-const themes = [
-  {
-    name: 'default',
-    colorPrimary: '#212529',
-    colorSecondary: '#495057',
-    colorTertiary: '#ced4da',
-    bgPrimary: '#dee2e6',
-    bgSecondary: '#f8f9fa'
-  },
-  {
-    name: 'dark',
-    colorPrimary: '#f8f9fa',
-    colorSecondary: '#dee2e6',
-    colorTertiary: '#ced4da',
-    bgPrimary: '#495057',
-    bgSecondary: '#212529'
-  },
-  {
-    name: 'candyfloss',
-    colorPrimary: '#fb6f92',
-    colorSecondary: '#ff8fab',
-    colorTertiary: '#ffb3c6',
-    bgPrimary: '#ffc2d1',
-    bgSecondary: '#ffe5ec'
-  },
-]
+// This function dynamically renders theme selection boxes within the Theme Selection Container div. Each theme selection box is given a unique id, a class of 'theme', a color palette, and includes an event listener so that when a theme is clicked - the applyTheme function is called.
 
 const addThemeSelector = () => {
-  let theme = {}
-  let themeId = ''
-  let appendAfter = document.getElementById('theme-heading')
-  let color = {}
+  let themeHeading = document.getElementById('theme-heading')
   
   for (let i = 0; i < themes.length; i++) {
-    
-    themeId = i
+    const themeId = i
+    let theme
 
     theme = document.createElement('div')
     theme.setAttribute('id', `theme-${i}`)
     theme.classList.add('theme')
     theme.setAttribute('value', themes[i].name)
-    theme.addEventListener('click', (e) => changeTheme(themes[i].name))
+    theme.addEventListener('click', (e) => applyTheme(themes[i].name))
 
     theme.innerHTML = `
       <h6 class="palette-heading">${themes[i].name}</h6>
       <div id="palette-${i}" class="colors"></div>
     `
 
-    appendAfter.appendChild(theme)
+    themeHeading.appendChild(theme)
 
-    let colorArray = [
+    const colorArray = [
       themes[i].colorPrimary,
       themes[i].colorSecondary,
       themes[i].colorTertiary,
-      themes[i].bgPrimary,
       themes[i].bgSecondary,
+      themes[i].bgPrimary,
     ]
 
     for (let i = 0; i <= 4; i++) {
-      color = document.createElement('div')
+      const color = document.createElement('div')
       color.classList.add('color')
 
       document.getElementById(`palette-${themeId}`).appendChild(color).style.backgroundColor = colorArray[i]
---
+++ mission-2/themes.js
@@ -0,0 +1,35 @@
+const themes = [
+  {
+    name: 'default',
+    colorPrimary: '#212529',
+    colorSecondary: '#495057',
+    colorTertiary: '#ced4da',
+    bgSecondary: '#dee2e6',
+    bgPrimary: '#f8f9fa'
+  },
+  {
+    name: 'dark',
+    colorPrimary: '#f8f9fa',
+    colorSecondary: '#dee2e6',
+    colorTertiary: '#ced4da',
+    bgSecondary: '#495057',
+    bgPrimary: '#212529'
+  },
+  {
+    name: 'candyfloss',
+    colorPrimary: '#fb6f92',
+    colorSecondary: '#ff8fab',
+    colorTertiary: '#ffb3c6',
+    bgSecondary: '#ffc2d1',
+    bgPrimary: '#ffe5ec'
+  },
+  {
+    name: 'forest',
+    colorPrimary: '#6c584c',
+    colorSecondary: '#a98467',
+    colorTertiary: '#adc178',
+    bgSecondary: '#dde5b6',
+    bgPrimary: '#f0ead2'
+  }
+]
+
